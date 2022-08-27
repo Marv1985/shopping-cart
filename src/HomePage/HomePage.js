31b186bbf5f10b16b69f719cd661e2e6e4ecf1bd
marvins85 <marvinshannon@hotmail.co.uk>
2022-08-27 15:09:16 +0100

    test add

---
+++ src/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import "./HomePage.css";
+import HomeHeader from "./components/HomeHeader";
+
+function HomePage() {
+  return (
+    <div>
+      <HomeHeader />
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ src/HomePage/components/HomeHeader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "/home/marv/the-odin-project/react-projects/shopping-cart/src/Images/game-logo.jpg";
+
+function HomeHeader() {
+  return (
+    <div>
+      <img src={logo} alt="logo" />
+    </div>
+  );
+}
+
+export default HomeHeader;
