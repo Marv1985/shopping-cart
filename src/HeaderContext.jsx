8de08e81db73de0e61c0be5adcfc209325d00a7d
marvin shannon <marvinshannon@hotmail.co.uk>
2024-12-28 15:50:14 +0000

    first commit

--- src/HeaderContext.jsx
+++ src/HeaderContext.jsx
@@ -6,9 +6,14 @@ export const HeaderProvider = ({ children }) => {
     // define state to be accessed via the useContext hook
     const [quantities, setQuantities] = useState({}); // Store quantities for each item by ID
 
-    // console.log(quantities);
-
-    // Sum up the quantities for all categories
+    // Extracts the values of the quantities object into an array
+    // Object.values returns an array of values from the quantities object
+    // reduce() iterates over the array of values to accumulate a result
+    // (acc, quantity) are the parameters of the callback function:
+    // - acc is the accumulator (stores the running total)
+    // - quantity is the current value being processed
+    // acc + quantity adds the current value to the accumulator
+    // 0 is the initial value of the accumulator (start sum at 0)
     const totalQuantity = Object.values(quantities).reduce((acc, quantity) => acc + quantity, 0);
 
     // Handle quantity change for a specific category
@@ -21,6 +26,13 @@ export const HeaderProvider = ({ children }) => {
             };
         });
     };
+    
+  function add(){
+    const sum = 2+2
+    function addup() {
+        return sum + 5
+    }
+  }
 
     return (
         <HeaderContext.Provider value={{ quantities, totalQuantity, handleQuantityBlur }}>
